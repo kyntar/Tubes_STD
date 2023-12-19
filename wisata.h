36e57cd4c3bc6c6a1aeb792adab33ad9ff84c31c
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2023-12-19 20:53:30 +0700

    Initial commit

---
+++ wisata.h
@@ -0,0 +1,12 @@
+#ifndef WISATA_H_INCLUDED
+#define WISATA_H_INCLUDED
+#include <iostream>
+#define info(P)(P)->info
+#define next(P)(P)->next
+#define first(L)(L).first
+#define last(L)(L).last
+
+
+
+
+#endif // WISATA_H_INCLUDED
